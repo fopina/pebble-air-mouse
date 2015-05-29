125da1cd6fa912875c6b4d5b9a37ced766133943
Filipe Pina <fopina@skmobi.com>
2015-05-29 16:14:06 +0100

    server based on https://github.com/Lawouach/WebSocket-for-Python/blob/master/example/echo_cherrypy_server.py

---
+++ desktop/cherrypy_server.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import argparse
+import random
+import cherrypy
+
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.websocket import WebSocket
+from ws4py.messaging import TextMessage
+
+
+class AirMouseWebSocketHandler(WebSocket):
+    def received_message(self, m):
+        print m
+
+    def closed(self, code, reason="No reason"):
+        print code, reason
+
+
+class Root(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.scheme = 'ws'
+
+    @cherrypy.expose
+    def index(self):
+        return """<html>
+    <head>
+      <script type='application/javascript' src='http://code.jquery.com/jquery-2.1.4.min.js'></script>
+      <script type='application/javascript'>
+        $(document).ready(function() {
+
+          websocket = '%(scheme)s://%(host)s:%(port)s/ws';
+          if (window.WebSocket) {
+            ws = new WebSocket(websocket);
+          }
+          else if (window.MozWebSocket) {
+            ws = MozWebSocket(websocket);
+          }
+          else {
+            console.log('WebSocket Not Supported');
+            return;
+          }
+
+          window.onbeforeunload = function(e) {
+            ws.close(1000, 'OUT!');
+
+            if(!e) e = window.event;
+            e.stopPropagation();
+            e.preventDefault();
+          };
+          ws.onmessage = function (evt) {
+             $('#log').val($('#log').val() + evt.data + '\\n');
+          };
+          ws.onopen = function() {
+            $('#log').val($('#log').val() + 'Connected\\n');
+          };
+          ws.onclose = function(evt) {
+             $('#log').val($('#log').val() + 'Connection closed by server: ' + evt.code + ' \"' + evt.reason + '\"\\n');
+          };
+
+          $('#send').click(function() {
+             console.log($('#message').val());
+             ws.send($('#message').val());
+             $('#message').val("");
+             return false;
+          });
+        });
+      </script>
+    </head>
+    <body>
+    <form action='#' id='logform' method='get'>
+      <textarea id='log' cols='35' rows='10'></textarea>
+      <br />
+      <label for='message'>Message: </label><input type='text' id='message' />
+      <input id='send' type='submit' value='Send' />
+      </form>
+    </body>
+    </html>
+    """ % {'host': self.host, 'port': self.port, 'scheme': self.scheme}
+
+    @cherrypy.expose
+    def ws(self):
+        cherrypy.log("Handler created: %s" % repr(cherrypy.request.ws_handler))
+
+if __name__ == '__main__':
+    import logging
+    from ws4py import configure_logger
+    configure_logger(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser(description='Pebble-Air-Mouse Server')
+    parser.add_argument('--host', default='127.0.0.1')
+    parser.add_argument('-p', '--port', default=9000, type=int)
+    args = parser.parse_args()
+
+    cherrypy.config.update({
+        'server.socket_host': args.host,
+        'server.socket_port': args.port,
+    })
+
+    WebSocketPlugin(cherrypy.engine).subscribe()
+    cherrypy.tools.websocket = WebSocketTool()
+
+    cherrypy.quickstart(Root(args.host, args.port), '', config={
+        '/ws': {
+            'tools.websocket.on': True,
+            'tools.websocket.handler_cls': AirMouseWebSocketHandler
+        },
+    })
